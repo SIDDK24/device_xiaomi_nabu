f75e4bb034975090798de37765d424e516fb191f
MD Raza <raza231198@gmail.com>
2023-07-08 23:16:36 +0530

    nabu: Update gpt-utils & bootctrl
    Signed-off-by: MD Raza <raza231198@gmail.com>
    Change-Id: I3bb6536cc4f53ce39c7e1389fb672ee478143392

--- device.mk
+++ device.mk
@@ -34,26 +34,38 @@ LOCAL_PATH := device/xiaomi/nabu
 PRODUCT_SHIPPING_API_LEVEL := 30
 
 # A/B
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.1-impl-qti \
+    android.hardware.boot@1.1-impl-qti.recovery \
+    android.hardware.boot@1.1-service \
+    bootctrl.msmnile \
+    bootctrl.msmnile.recovery
+
+PRODUCT_PACKAGES_DEBUG += \
+    bootctl
+
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
 
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_vendor=true \
+    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
+    FILESYSTEM_TYPE_vendor=ext4 \
+    POSTINSTALL_OPTIONAL_vendor=true
+
 PRODUCT_PACKAGES += \
+  update_engine \
+  update_engine_sideload \
+  update_verifier
+
+PRODUCT_PACKAGES += \
+    checkpoint_gc \
     otapreopt_script
 
-# Boot control HAL
-PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-impl \
-    android.hardware.boot@1.2-service \
-    android.hardware.boot@1.2-impl-wrapper.recovery \
-    android.hardware.boot@1.2-impl-wrapper \
-    android.hardware.boot@1.2-impl.recovery \
-    bootctrl.msmnile.recovery
-
-PRODUCT_PACKAGES_DEBUG += \
-    bootctl
+PRODUCT_PACKAGES_DEBUG += update_engine_client
 
 # Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
